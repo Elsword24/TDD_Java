8db71a09a5412c7641b0cc592e4087ff739323ea
Quentin Launay <50755766+Elsword24@users.noreply.github.com>
2023-03-16 16:54:36 +0100

    Update GameControllerTest.java

--- src/test/java/com/epsi/TDD/controller/GameControllerTest.java
+++ src/test/java/com/epsi/TDD/controller/GameControllerTest.java
@@ -8,6 +8,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
